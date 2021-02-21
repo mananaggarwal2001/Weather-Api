30944d65ce9362f06e34e1188a47d8951d9322c7
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-02-21 20:21:21 +0530

    Sixth Commit

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const bodyparser = require("body-parser");
 
 
 app.use(bodyparser.urlencoded({extended:true}));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.get("/" , function(req , res){
 res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/" , function(req , res){
 const query = req.body.cityName;
+
 const url ="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=062798097c085e918908ddb409a14c57&units=metric";
   https.get(url,function(response){
     console.log(response.statusCode);
@@ -21,17 +22,39 @@ const url ="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=
     response.on('data',function(data){
       const variable = JSON.parse(data);
       console.log(variable);
-      console.log(data);
 
       var desc = variable.main.temp;
-      console.log(desc);
+      // console.log(desc);
       var weatherDescription = variable.weather[0].description;
       var weatherIcon = variable.weather[0].icon;
+      let latitude = variable.coord.lat;
+      let longitude = variable.coord.lon;
+
+      let cityName = variable.name;
+      let countryName = variable.sys.country;
+      switch (textBoxValue) {
+        case 'coordinateFormat':{
+        let cityValue = $('#cityInput').val();
+        cityValue = latitude;
+
+        let countryValue = $('#countryValue').val();
+        countryValue = longitude;
+
+         break;
+        }
+        // case 'nameFormat'{
+        //
+        // break;
+        // }
+        //
+        default:
+
+      }
+
       res.send("<h1>The Temprature of the "+ query + "  is: " + desc + "</h1>" + "\n"+ "The Weather Condition in "+ query +" is " + weatherDescription);  // res is used for the response to the client site.
     })
   })
 });
-
 
 app.listen(3000,function(){
   console.log("Server is Running on listening on port 3000");
