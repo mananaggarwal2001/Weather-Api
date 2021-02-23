b380a2d05b7e65f5bad5847e14e37cd100e8862d
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-02-24 01:44:58 +0530

    Seven Commit

--- app.js
+++ app.js
@@ -6,56 +6,46 @@ const https = require('https');
 const bodyparser = require("body-parser");
 
 
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.get("/" , function(req , res){
-res.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/" , function(req , res){
-const query = req.body.cityName;
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
 
-const url ="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=062798097c085e918908ddb409a14c57&units=metric";
-  https.get(url,function(response){
-    console.log(response.statusCode);
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=062798097c085e918908ddb409a14c57&units=metric";
+    https.get(url, function(response) {
+        console.log(response.statusCode);
 
-    response.on('data',function(data){
-      const variable = JSON.parse(data);
-      console.log(variable);
+        response.on('data', function(data) {
+            const variable = JSON.parse(data);
+            console.log(variable);
 
-      var desc = variable.main.temp;
-      // console.log(desc);
-      var weatherDescription = variable.weather[0].description;
-      var weatherIcon = variable.weather[0].icon;
-      let latitude = variable.coord.lat;
-      let longitude = variable.coord.lon;
+            var desc = variable.main.temp;
+            // console.log(desc);
+            var weatherDescription = variable.weather[0].description;
+            var weatherIcon = variable.weather[0].icon;
+            let latitude = variable.coord.lat;
+            let longitude = variable.coord.lon;
 
-      let cityName = variable.name;
-      let countryName = variable.sys.country;
-      switch (textBoxValue) {
-        case 'coordinateFormat':{
-        let cityValue = $('#cityInput').val();
-        cityValue = latitude;
+            let cityName = variable.name;
+            let countryName = variable.sys.country;
 
-        let countryValue = $('#countryValue').val();
-        countryValue = longitude;
+            // case 'nameFormat'{
+            //
+            // break;
+            // }
+            //
 
-         break;
-        }
-        // case 'nameFormat'{
-        //
-        // break;
-        // }
-        //
-        default:
 
-      }
 
-      res.send("<h1>The Temprature of the "+ query + "  is: " + desc + "</h1>" + "\n"+ "The Weather Condition in "+ query +" is " + weatherDescription);  // res is used for the response to the client site.
+            res.send("<h1>The Temprature of the " + query + "  is: " + desc + "</h1>" + "\n" + "The Weather Condition in " + query + " is " + weatherDescription); // res is used for the response to the client site.
+        })
     })
-  })
 });
 
-app.listen(3000,function(){
-  console.log("Server is Running on listening on port 3000");
+app.listen(3000, function() {
+    console.log("Server is Running on listening on port 3000");
 })
--- public/Weather.js
+++ public/Weather.js
@@ -1,25 +1,81 @@
-$('input[type="radio"]').click(function(){
-  let value = $('input[name="Format"]:checked').val();
-  switch (value) {
-  case 'coordinateFormat':{
-  $('#cityName').text("Latitude");
-  $('#countryName').text("Longitude");
-  $('#cityInput').attr('placeholder','Latitude');
-  $('#countryInput').attr('Placeholder','Longitude');
+$('input[type="radio"]').click(function() {
+    let value = $('input[name="Format"]:checked').val();
+    switch (value) {
+        case 'coordinateFormat':
+            {
+                $('#cityName').text("Latitude");
+                $('#countryName').text("Longitude");
+                $('#cityInput').attr('placeholder', 'Latitude');
+                $('#countryInput').attr('Placeholder', 'Longitude');
 
-  break;
-  }
-  case 'nameFormat':{
-    $('#cityName').text("City Name");
-    $('#countryName').text("Country Name");
+                break;
 
-    $('#cityInput').attr('placeholder','City Name');
-    $('#countryInput').attr('Placeholder','Country Name');
+            }
+        case 'nameFormat':
+            {
+                $('#cityName').text("City Name");
+                $('#countryName').text("Country Name");
+                $('#cityInput').attr('placeholder', 'City Name');
+                $('#countryInput').attr('Placeholder', 'Country Name');
+                break;
+            }
 
-  break;
-  }
-
-  default:
-  alert("Please Choose An Option");
-  }
+        default:
+            alert("Please Choose An Option");
+    }
 });
+
+$(".weatherForm").on(('submit'), function() { // on method specifies the current event that was happening 
+    console.log('trigger');
+
+    let that = $(this),
+        data = {}; // appending this through jquery mwthod because we have been passing onto the ajax through json method in the data variable
+
+    that.find('[name]').each(function(index, forValue) { // Find Method Used to find Each Element With the name attribute in the form
+        let that = $(this),
+            name = that.attr('name'),
+            formValue = that.val();
+
+        data[name] = formValue;
+
+    })
+
+
+
+    const xml = new XMLHttpRequest;
+    let query = data.cityName;
+    xml.open('POST', "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=062798097c085e918908ddb409a14c57&units=metric", true)
+
+    xml.onload = function() {
+        let data = JSON.parse(this.responseText);
+        console.log(data);
+
+        let latitude = data.coord.lat;
+        let longitude = data.coord.lon;
+        let countryName = data.sys.country;
+        let temprature = data.main.temp;
+        let cityName = data.name;
+
+        let radiovalue = $('input[name="Format"]:checked').val();
+        if (radiovalue === 'coordinateFormat') {
+
+            let cityNameValue = $('#cityInput').val();
+            console.log(cityNameValue);
+
+
+
+
+        } else if (radiovalue === "nameFormat") {
+            $('#cityInput').attr('value', cityName);
+            $('#countryInput').attr('value', countryName);
+        }
+
+
+
+        $('.tempatureResultClass').text(" The Temprature of " + cityName + " is :- " + temprature);
+
+
+    }
+    xml.send();
+    return false; // to Prevent it from Submitting to the Default State
+})
